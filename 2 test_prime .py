41c78780751b432e191378ecf87d9c0c79d5881b
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-02-01 15:12:39 +0600

    remove " ; "

--- 2 test_prime .py
+++ 2 test_prime .py
@@ -2,7 +2,7 @@ def test_prime(n):
     if (n==1):
         return False
     elif (n==2):
-        return True;
+        return True
     else:
         for x in range(2,n):
             if(n%x==0):
